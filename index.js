89cfe65516bbc8fb326360185b07b438107adfd5
Kaitlyn Williamson <kaitlyn.shae28@gmail.com>
2023-09-29 14:24:43 -0400

    y

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = { name, email };
+    const userDataStringified = JSON.stringify(userData);
+  
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: userDataStringified
+    })
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        const idElement = document.createElement('p');
+        idElement.textContent = `New ID: ${id}`;
+        document.body.appendChild(idElement);
+      })
+      .catch(error => {
+        const errorElement = document.createElement('p');
+        errorElement.textContent = `Error: ${error.message}`;
+        document.body.appendChild(errorElement);
+      });
+  }  
